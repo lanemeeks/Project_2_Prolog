69943ec7e4d9126c4c8db0e50a615870154ccd66
Lane Meeks <147874358+lanemeeks@users.noreply.github.com>
2026-05-06 11:32:19 -0500

    5/6 - session end

--- plan.pl
+++ plan.pl
@@ -2,29 +2,29 @@
 workstation_idle(_,_) :- fail.
 avoid_workstation(_,_) :- fail.
 avoid_shift(_,_) :- fail.
-% so these above statements will be false if they don't exist instead of crashing
+% so these above statements will be false if they dont exist instead of crashing
 
 % main plan predicate
 plan() :-
 %
-%define me :(
+% define me :(
 % assign employees to shifts
 % build schedule moring
 % build scheudle evening
 % build schedule night
 % validate all three schedules then its good
 
-%base case for the recursive rule below
+% base case for the recursive rule below
 assign_all_employees([], []).
 
-%assigning employees to shifts, recursive rule
+% assigning employees to shifts, recursive rule
 assign_all_employees([E | Rest], [assigned(E, Shift, Workstation) | Assignments]) :- 
     shift(Shift),
     workstation(Workstation, _, _),
     can_work(E, Shift, Workstation),
     assign_all_employees(Rest, Assignments).
 
-%defining shifts
+% defining shifts
 shift(morning).
 shift(evening).
 shift(night).
@@ -34,4 +34,37 @@ can_work(Employee, Shift, Workstation) :-
     \+ avoid_shift(Employee, Shift),
     \+ avoid_workstation(Employee, Workstation),
     \+ workstation_idle(Workstation, Shift).
+
+% ex: build_shift_schedule(morning, Assignments, Morning)
+build_shift_schedule(Shift, Assignments, Schedule) :-
+    findall(
+        workstation(W, Employees),
+        employees_at_workstation(Shift, W, Assignments, Employees),
+        PreSchedule
+    ),
+    remove_empty_workstations(PreSchedule, Schedule).
+
+%finds all employees assigned to workstation W during shift Shift
+employees_at_workstation(Shift, W, Assignments, Employees) :-
+    workstation(W, _, _),
+    \+ workstation_idle(W, Shift),
+    findall(
+        E,
+        member(assigned(E, Shift, W), Assignments),
+        Employees
+    ).
+
+
+% recursively removing empty workstations from the schedule
+% base case
+remove_empty_workstations([], []).
+
+% case for workstation with no employees
+remove_empty_workstations([workstation(_, []) | Rest], Cleaned) :-
+    remove_empty_workstations(Rest, Cleaned).
+
+% regular case for a workstation that may have employees
+remove_empty_workstations([workstation(W, Employees) | Rest], [workstation(W, Employees) | Cleaned]) :-
+    Employees \= [],
+    remove_empty_workstations(Rest, Cleaned).
 
